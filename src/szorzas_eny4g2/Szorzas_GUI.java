0d942bd3b5848b35923fc4c2dfcc82a8d1a35fdf
Balogh Szilveszter <szili@szili-iMac.local>
2018-06-14 03:26:12 +0200

    11_JProgressBar_hozzaadasa

--- src/szorzas_eny4g2/Szorzas_GUI.java
+++ src/szorzas_eny4g2/Szorzas_GUI.java
@@ -8,16 +8,22 @@ package szorzas_eny4g2;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.lang.reflect.InvocationTargetException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.scene.control.ProgressBar;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import static javax.swing.JOptionPane.INFORMATION_MESSAGE;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import javax.swing.JSpinner;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
 import javax.swing.SpinnerNumberModel;
+import javax.swing.SwingUtilities;
 
 /**
  *
@@ -36,9 +42,10 @@ public class Szorzas_GUI {
     private final JLabel szorzasLabel = new JLabel("*");
     private JSpinner secondSpinner = new JSpinner(new SpinnerNumberModel(0, 0, Integer.MAX_VALUE, 1));
     private final JButton szamitasButton = new JButton("Számítás!");
+    private JProgressBar progressBar = new JProgressBar();
     
     public Szorzas_GUI() {
-        frame.setSize(500, 500);
+        frame.setSize(800, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new FlowLayout());
         frame.add(tabbedPane);
@@ -50,6 +57,11 @@ public class Szorzas_GUI {
         szorzasPanel.add(secondSpinner);
         szorzasPanel.add(szamitasButton);
         
+        progressBar.setBounds(50, 50, 250, 30);
+        progressBar.setValue(0);
+        progressBar.setStringPainted(true);
+        szorzasPanel.add(progressBar);
+        
         aboutPanel.add(aboutText);
         
         frame.setVisible(true);
@@ -59,10 +71,31 @@ public class Szorzas_GUI {
             public void actionPerformed(ActionEvent e) {
                 int firstValue = (int)firstSpinner.getValue();
                 int secondValue = (int)secondSpinner.getValue();
+                updateBar();
                 JOptionPane.showMessageDialog(frame, "A szorzás eredménye: " + Szorzas_ENY4G2.multiplyNaturals(firstValue,secondValue), "Szorzateredmény", INFORMATION_MESSAGE);
             }
         });
         
     }
-}
+            private void updateBar() {
+                    Thread t = new Thread(() -> {
+            try {
+                int i = 0;
+                while(i<=100){
+                    final int step = i+1;
+                    Thread.sleep(10);
+                    SwingUtilities.invokeAndWait(() -> {
+                        progressBar.setValue(step+1);
+                    });
+                    i++;
+                }
+                
+            } catch (InterruptedException | InvocationTargetException ex) {
+                Logger.getLogger(Szorzas_GUI.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        });
+        t.start();
+    }
+        }
+
 
